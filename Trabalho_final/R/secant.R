b5fb5fd4a3b732e46a911967e05b064863ab5a81
Laôni A. Moreira <laonimoreira.capanema@gmail.com>
2025-01-26 00:08:49 -0300

    fix: rename secant function arguments

--- Trabalho_final/R/secant.R
+++ Trabalho_final/R/secant.R
@@ -4,5 +4,5 @@ require(pracma)
 f <- function(x) x^3 - x - 2
 
 # Testando o Método da Secante
-tempo <- system.time(replicate(100, secant(f, x0 = 1.5, x1 = 2, tol = 1e-6)))
+tempo <- system.time(replicate(1000000, secant(f, a = 1.5, b = 2, tol = 1e-6)))
 print(tempo)
